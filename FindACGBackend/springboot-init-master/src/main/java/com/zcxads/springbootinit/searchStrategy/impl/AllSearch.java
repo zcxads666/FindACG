929260aa6403a46ac36e7de874b00317d9f74ed9
Hrzmxn <1316510990@qq.com>
2024-11-19 00:01:42 +0800

    11.19

--- FindACGBackend/springboot-init-master/src/main/java/com/zcxads/springbootinit/searchStrategy/impl/AllSearch.java
+++ FindACGBackend/springboot-init-master/src/main/java/com/zcxads/springbootinit/searchStrategy/impl/AllSearch.java
@@ -1,9 +1,13 @@
 package com.zcxads.springbootinit.searchStrategy.impl;
 
 import com.zcxads.springbootinit.searchStrategy.SearchStrategy;
+import com.zcxads.springbootinit.spider.adaptee.GameAdaptee;
+import com.zcxads.springbootinit.spider.adaptee.NovelAdaptee;
+import com.zcxads.springbootinit.spider.adapter.spiderAdapter;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -14,7 +18,18 @@ public class AllSearch implements SearchStrategy {
 
     @Override
     public List<Map<String, Object>> doSearch(String text) {
-        System.out.println("all search");
-        return null;
+        spiderAdapter novelSearch = new spiderAdapter(new NovelAdaptee());
+        List<Map<String, Object>> searchNovel = novelSearch.doSearchNovel(text);
+        spiderAdapter animeSearch = new spiderAdapter(new GameAdaptee());
+        List<Map<String, Object>> searchGame = animeSearch.doSearchGame(text);
+        List<Map<String, Object>> combinedList = new ArrayList<>();
+
+    // 将 searchNovel 的所有元素添加到 combinedList 中
+    combinedList.addAll(searchNovel);
+
+    // 将 searchGame 的所有元素添加到 combinedList 中
+    combinedList.addAll(searchGame);
+
+        return combinedList;
     }
 }
